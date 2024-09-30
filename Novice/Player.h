f6f19f1fa0c8489af7ccafe1084f511aa8c79b01
cho-yohan <k023g1037@g.neec.ac.jp>
2024-09-30 16:51:03 +0900

    コマンドパタン

---
+++ Novice/Player.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Vector2.h"
+
+class Player {
+public:
+	
+	Vector2 pos_;
+	int radius_;
+	int speed_;
+
+public:
+	Player(Vector2 pos, int radius, int speed);
+	~Player();
+	void Update(char* keys, char* preKeys);
+	void Draw();
+};
